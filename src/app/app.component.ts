d5315ad127c828aa789dba65429b10577ab2ccfc
Marcel Menke <marcel.menke1981@gmail.com>
2024-09-30 13:05:09 +0200

    added icons header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./shared/components/header/header.component";
+import { HeroComponent } from "./hero/hero.component";
+import { MainContentComponent } from "./main-content/main-content.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, 
+    HeaderComponent, 
+    HeroComponent, 
+    MainContentComponent,
+    CommonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
