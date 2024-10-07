08ea74440858bfffb331f05663bc42d8655008b7
Marcel Menke <marcel.menke1981@gmail.com>
2024-10-07 11:56:32 +0200

    added translate to about me and skill

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./shared/components/header/header.component";
 import { HeroComponent } from "./hero/hero.component";
@@ -9,22 +9,37 @@ import { FooterComponent } from "./shared/components/footer/footer.component";
 import { AboutMeComponent } from './about-me/about-me.component';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { SkillSectionComponent } from "./skill-section/skill-section.component";
+import { PLATFORM_ID } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,
+  imports: [
+    RouterOutlet,
     HeaderComponent,
     HeroComponent,
     MainContentComponent,
     AboutMeComponent,
     CommonModule,
     LinksComponent,
-    FooterComponent, SkillSectionComponent],
+    FooterComponent, 
+    SkillSectionComponent,
+    TranslateModule
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
-
 export class AppComponent {
   title = 'portfolio';
+
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    private router: Router,
+    private translate: TranslateService
+  ) {
+    // Set default language
+    this.translate.setDefaultLang('en');
+    this.translate.use('en');
+  }
 }
