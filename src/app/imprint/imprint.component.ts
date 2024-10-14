8bb56e8aacf62a7ed30ce8258d2974ebb111fb0c
Marcel Menke <marcel.menke1981@gmail.com>
2024-10-14 11:08:46 +0200

    changed translate handling in imprint component

--- src/app/imprint/imprint.component.ts
+++ src/app/imprint/imprint.component.ts
@@ -1,19 +1,33 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-imprint',
   standalone: true,
-  imports: [ CommonModule, TranslateModule  
-  ],
+  imports: [ CommonModule, TranslateModule ],
   templateUrl: './imprint.component.html',
-  styleUrl: './imprint.component.scss'
+  styleUrls: ['./imprint.component.scss']
 })
 
-export class ImprintComponent {
-  constructor(private translate: TranslateService) {
-    this.translate.use(this.currentLanguage);
+export class ImprintComponent implements OnInit, OnDestroy {
+  currentLanguage: 'en' | 'de' = 'en';
+  private languageSubscription: Subscription | null = null;
+
+  constructor(private translate: TranslateService) {}
+
+  ngOnInit(): void {
+    this.currentLanguage = this.translate.currentLang as 'en' | 'de';
+
+    this.languageSubscription = this.translate.onLangChange.subscribe((event) => {
+      this.currentLanguage = event.lang as 'en' | 'de';
+    });
   }
-  currentLanguage: 'en' | 'de' = 'en'
+
+  ngOnDestroy(): void {
+    if (this.languageSubscription) {
+      this.languageSubscription.unsubscribe();
+    }
+  }
 }
