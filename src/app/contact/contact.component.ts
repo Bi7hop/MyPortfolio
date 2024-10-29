1360f6a35f405cdf420a868eab07bce997a4f6f0
Marcel Menke <marcel.menke1981@gmail.com>
2024-10-29 11:44:36 +0100

    routerlink to contact

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -2,12 +2,13 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule, HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CommonModule, TranslateModule, ReactiveFormsModule, HttpClientModule],
+  imports: [CommonModule, TranslateModule, ReactiveFormsModule, HttpClientModule, RouterLink],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
