539a104e411118230b949d575d22e98f4cbd3c1f
Marcel Menke <marcel.menke1981@gmail.com>
2024-10-29 09:11:42 +0100

    changed submitform in contact.ts

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -7,19 +7,28 @@ import { TranslateModule, TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [ CommonModule, TranslateModule, ReactiveFormsModule, HttpClientModule],
+  imports: [CommonModule, TranslateModule, ReactiveFormsModule, HttpClientModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
 
-
   contactForm!: FormGroup;
   messageSent: boolean = false;
   errorMessage: string = '';
 
+  post = {
+    endPoint: 'https://marcel-menke.info/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
 
-  constructor(private http: HttpClient, private fb: FormBuilder,public translateService: TranslateService) {
+  constructor(private http: HttpClient, private fb: FormBuilder, public translateService: TranslateService) {
     this.contactForm = this.fb.group({
       name: ['', [Validators.required]],
       email: ['', [Validators.required, Validators.email]],
@@ -28,17 +37,32 @@ export class ContactComponent {
     });
   }
 
-
   submitForm() {
     if (this.contactForm.valid) {
-      console.log('Formular erfolgreich gesendet:', this.contactForm.value);
-
-      this.messageSent = true;
-      this.contactForm.reset();
+      const formData = this.contactForm.value;
+      this.http.post(this.post.endPoint, this.post.body(formData), this.post.options)
+        .subscribe({
+          next: () => {
+            console.log('Formular erfolgreich gesendet:', formData);
+            this.messageSent = true;
+            this.contactForm.reset();
+            this.hideSuccessMessageAfterDelay();
+          },
+          error: (error) => {
+            this.errorMessage = 'Es gab ein Problem beim Senden der Nachricht.';
+            console.error(error);
+          }
+        });
     } else {
       this.errorMessage = 'Bitte füllen Sie alle Felder korrekt aus und stellen Sie sicher, dass die Nachricht mindestens 10 Wörter enthält.';
       console.log('Formular ungültig:', this.contactForm.errors);
     }
+  }
+
+  hideSuccessMessageAfterDelay() {
+    setTimeout(() => {
+      this.messageSent = false;
+    }, 5000);
   }
 
   wordCountValidator(minWords: number) {
@@ -49,12 +73,11 @@ export class ContactComponent {
     };
   }
 
-  scrollToTop():void{
-    window.scroll({ 
-      top: 0, 
-      left: 0, 
-      behavior: 'smooth' 
+  scrollToTop(): void {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
     });
   }
-
 }
